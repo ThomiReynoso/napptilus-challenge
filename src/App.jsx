9aa5a859b3027ead83afe46d6ca97882de1b770e
Thomas Reynoso <thomas@mudafy.com>
2023-03-18 16:06:46 -0300

    ignore eslint for all project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProductDetails from './pages/productDetails';
 import ProductList from './pages/productList';
