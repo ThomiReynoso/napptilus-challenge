81bce381f362a29d4ecf2d4a64872954607c6745
Thomas Reynoso <thomas@mudafy.com>
2023-03-18 15:24:50 -0300

    add eslint to project

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProductDetails from './pages/productDetails';
 import ProductList from './pages/productList';
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProductList/>} />
-        <Route path="/product/:id" element={<ProductDetails/>} />
+        <Route path="/" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
     </BrowserRouter>
   );
