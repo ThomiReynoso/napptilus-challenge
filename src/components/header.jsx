a22cc8d12b6cb4aa4bc4c70b08b011a9247a1289
Thomas Reynoso <thomas@mudafy.com>
2023-03-19 17:54:38 -0300

    add syles to header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,78 @@
-import { Button, Text } from '@chakra-ui/react';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+	Icon,
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import {CgShoppingCart} from 'react-icons/cg'
 
-const Header = ({ itemCount }) => {
+const Links = [ 'Products'];
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'/'}>
+    {children}
+  </Link>
+);
+
+export default function Header({ itemCount }) {
+
+	const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <header>
-      <Link to="/">Mobile Shop</Link>
-      <nav>
-        <Link to="/">Productos</Link>
-      </nav>
-      <div>
-        <Text>Carrito: {itemCount}</Text>
-      </div>
-    </header>
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Link href='/' fontWeight={'bold'}>MOBILFY</Link>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+								))}
+             </HStack>
+							<Box fontWeight={'bold'}>Hacer breadcrumb</Box>	
+          </HStack>
+          <Flex alignItems={'center'}>
+            <Icon
+              as={CgShoppingCart}
+            />
+            ({itemCount})
+          </Flex>
+        </Flex>
+      </Box>
+    </>
   );
-};
-
-export default Header;
+}
