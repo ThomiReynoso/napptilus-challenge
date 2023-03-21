f4e811eccd545fe9c95abc1df1740cc6eae9d658
Thomas Reynoso <thomas@mudafy.com>
2023-03-21 13:59:49 -0300

    fix test warning

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import * as ProductService from '../src/services/product.service';
 
+const mockedProducts = [
+  {
+    id: 1,
+    brand: 'Acer',
+    model: 'Iconia Talk S',
+  },
+  {
+    id: 2,
+    brand: 'Acer',
+    model: 'Liquid Z6',
+  },
+];
+
 // Mock the fetch functions in range.service
 jest.mock('../src/services/product.service', () => ({
-  fetchAllProducts: jest.fn(),
+  fetchAllProducts: jest.fn(() => mockedProducts),
   fetchProduct: jest.fn(),
   addProductToCart: jest.fn(),
 }));
@@ -18,12 +31,16 @@ describe('App', () => {
     jest.clearAllMocks();
   });
   
-  test('renders logo', () => {
-    render(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
+  test('renders logo', async () => {
+    // I need to wrapp the component in act because it was throwing a warning when running tests
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      );
+    });
+
     const linkElement = screen.getByText(/mobilfy/i);
     expect(linkElement).toBeInTheDocument();
   });
