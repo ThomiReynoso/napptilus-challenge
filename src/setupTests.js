2fcf9133a5fc38c04f6b5f1a6afaf5213aafc38c
Thomas Reynoso <thomas@mudafy.com>
2023-03-21 12:13:58 -0300

    fix lint

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,26 +5,25 @@
 import '@testing-library/jest-dom';
 
 class LocalStorageMock {
-    constructor() {
-      this.store = {};
-    }
-  
-    getItem(key) {
-      return this.store[key] || null;
-    }
-  
-    setItem(key, value) {
-      this.store[key] = String(value);
-    }
-  
-    removeItem(key) {
-      delete this.store[key];
-    }
-  
-    clear() {
-      this.store = {};
-    }
+  constructor() {
+    this.store = {};
   }
-  
-  global.localStorage = new LocalStorageMock();
-  
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+
+  clear() {
+    this.store = {};
+  }
+}
+
+global.localStorage = new LocalStorageMock();
