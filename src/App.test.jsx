56feaa8097bb4446f02d7183dbb710a4351d3590
Thomas Reynoso <thomas@mudafy.com>
2023-03-21 01:31:52 -0300

    fixed warning of not created fetchs methods

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,13 +3,29 @@ import App from './App';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import * as ProductService from '../src/services/product.service';
 
-test('renders learn react link', () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-  const linkElement = screen.getByText(/mobilfy/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// Mock the fetch functions in range.service
+jest.mock('../src/services/product.service', () => ({
+  fetchAllProducts: jest.fn(),
+  fetchProduct: jest.fn(),
+  addProductToCart: jest.fn(),
+}));
+
+describe('App', () => {
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  
+  test('renders logo', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    const linkElement = screen.getByText(/mobilfy/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+})
